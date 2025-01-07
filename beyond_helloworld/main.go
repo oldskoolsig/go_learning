88b4c40da36f89ff1b9f942fa8e62891d59a6bf7
oldskoolsig <callmeshawn@gmail.com>
2025-01-07 14:39:28 -0500

    showing factored syntax, functions that return multiple values, and shorted syntax to declare and initialize variables

--- beyond_helloworld/main.go
+++ beyond_helloworld/main.go
@@ -1,7 +1,25 @@
 package main 
 
-import "fmt"
+// using factored style to group import statements 
+import (
+  "fmt"
+)
 
+/*
+  this factored style can be applied to function parameters
+    In the example of the add function, which accepts two integers, the function definition
+    can be shorted from
+      func add (x int, y int) int to:
+      func add (x, y int) int
+*/ 
+func add (x, y int) int {
+  return x + y
+}
+
+func swap (first string, second string) (string, string) {
+  return second, first 
+}
+  
 func greet_name (name string) string {
   greeted_name := fmt.Sprintf("Welcome, %v!", name)
   return greeted_name
@@ -9,4 +27,8 @@ func greet_name (name string) string {
 
 func main () {
   fmt.Println(greet_name("Shawn"))
+  a, b := swap("first", "second")
+  fmt.Println(a,b)
+
+  fmt.Println("Calling add func, passing 1 and 2, printing the sum: ", add(1,2))
 }
